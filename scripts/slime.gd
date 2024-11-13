0a5115b719466a8ee042225e0ccbd786d28b4e36
Tavyah <94623622+Tavyah@users.noreply.github.com>
2024-11-13 21:47:21 +0100

    slimes will no longer fall walk in air

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -4,17 +4,20 @@ const SPEED = 60
 var direction = 1
 @onready var ray_cast_2d_right: RayCast2D = $RayCast2DRight
 @onready var ray_cast_2d_left: RayCast2D = $RayCast2DLeft
+@onready var ray_cast_2d_right_down: RayCast2D = $RayCast2DRightDown
+@onready var ray_cast_2d_left_down: RayCast2D = $RayCast2DLeftDown
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	move_enemy(delta)
-
+	
 func move_enemy(delta: float):
-	if ray_cast_2d_right.is_colliding():
+	if ray_cast_2d_right.is_colliding() or not ray_cast_2d_right_down.is_colliding():
 		direction = -1
 		animated_sprite_2d.flip_h = true
-	if ray_cast_2d_left.is_colliding():
+	if ray_cast_2d_left.is_colliding() or not ray_cast_2d_left_down.is_colliding():
 		direction = 1
 		animated_sprite_2d.flip_h = false
 	position.x += direction * SPEED * delta
